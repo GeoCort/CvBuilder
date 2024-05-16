9bccddfea9c7edfdd7f1cf2321a14c092dec17ec
George Cortes <93002810+GeoCort@users.noreply.github.com>
2024-05-16 12:11:30 -0700

    added tailwind

--- cvbuilder/src/App.jsx
+++ cvbuilder/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import ResumeHeader from "./ResumeHeader"
 
-import Header from './Header'
 function App() {
   return (
-    <div>
-    <Header />
+    <div className="bg-slate-500 rounded-lg w-96">
+    <ResumeHeader
+        firstName="George"
+        lastName="Cortes"
+        email="cortes.george93@icloud.com"
+        phoneNumber="+1-(510)-460-0905" />
     </div>
   )
 }
---
+++ cvbuilder/src/ResumeHeader.jsx
@@ -0,0 +1,10 @@
+function ResumeHeader(props){
+    return(
+        <div className="text-lg flex py-2 flex-col justify-center items-center">
+        <h1>{props.firstName + " " + props.lastName}</h1>
+        <h1>{props.email}</h1>
+        <h1 className="text-">{props.phoneNumber}</h1>
+        </div >
+    );
+}
+export default ResumeHeader;

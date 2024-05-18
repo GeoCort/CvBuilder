58b275c6a32dd6807ef24c63c08c6d7266930438
George Cortes <93002810+GeoCort@users.noreply.github.com>
2024-05-18 12:35:32 -0700

    Added skills tab with ResumeSkills jsx

---
+++ cvbuilder/src/components/ResumeSkills.jsx
@@ -0,0 +1,12 @@
+function ResumeSkills(props){
+    let {list} = props.list;
+    return(
+        <>
+            <h1>Skills</h1>
+            <ul>
+               {/* add some code in here */}
+            </ul>
+        </>
+    );
+}
+export default ResumeSkills
